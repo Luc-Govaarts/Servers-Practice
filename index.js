4ff64bd60dc839b114e7f524c227ffce8d7cad6d
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-05-14 13:07:45 +0200

    more express practice

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ function onRequest () {
     console.log('testing....')
 }
 
+const path = '/user/:name'
+
+const page =  `
+  <html>
+    <title>Home</title>
+    <h1>Welcome</h1>
+  
+  </html>`
+
+
+
 app.get(
-    '/test', // route to listen on
-    onRequest // callback runs when the route is requested
+    path, // route to listen on
+    (request, respons) => {
+      const message = `Welcome ${request.params.name}`
+      respons.send(message)
+    } // callback runs when the route is requested
   )
 
 const port = 3000
@@ -18,6 +32,3 @@ function onListen () {
   }
 
 app.listen(port, onListen)
-
-
-npm 
